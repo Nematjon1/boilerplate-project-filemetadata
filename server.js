971d3bac7ca1a9347cc8e118c4318bfcc62b7539
Nematjon <frank.sport.93@gmail.com>
2021-01-10 20:40:39 +0300

    Finish file upload route

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
 var app = express();
 
 app.use(cors());
@@ -9,6 +13,16 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post("/api/fileanalyse",upload("upfile"), (req, res) => {
+    const {upfile} = req.body;
+    console.log(req.body, req.file)
+    res.json({
+        name: req.file.filename,
+        type: req.file.mimetype,
+        size: req.file.size
+    })
 });
 
 
